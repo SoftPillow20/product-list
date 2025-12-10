1758051fb3291c9b72a8dec420329c2c5ba377e1
SoftPillow20 <Nobila.Bernie19@gmail.com>
2025-12-10 22:42:12 +0800

    "Refactor: components now have their own parent component folder"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import data from "./data.json";
-import ShoppingList from "./components/ShoppingList";
-import Products from "./components/Products";
-import Product from "./components/Product";
-import IsEmptyCart from "./components/IsEmptyCart";
-import ShoppingCart from "./components/ShoppingCart";
-import Order from "./components/Order";
-import OrderList from "./components/OrderList";
-import OrderCost from "./components/OrderCost";
-import OrderInfo from "./components/OrderInfo";
-import ProductOrder from "./components/ProductOrder";
-import OrderBtn from "./components/OrderBtn";
-import ProductOrderDetails from "./components/ProductOrderDetails";
+import ShoppingList from "./components/Shopping List/ShoppingList";
+import Products from "./components/Shopping List/Products";
+import Product from "./components/Shopping List/Product";
+import IsEmptyCart from "./components/Shopping Cart/IsEmptyCart";
+import ShoppingCart from "./components/Shopping Cart/ShoppingCart";
+import Order from "./components/Shopping Cart/Order";
+import OrderList from "./components/Shopping Cart/OrderList";
+import OrderCost from "./components/Shopping Cart/OrderCost";
+import OrderInfo from "./components/Shopping Cart/OrderInfo";
+import ProductOrder from "./components/Shopping List/ProductOrder";
+import OrderBtn from "./components/Shopping Cart/OrderBtn";
+import ProductOrderDetails from "./components/Shopping List/ProductOrderDetails";
+import Modal from "./components/Modal/Modal";
+import ConfirmOrderModal from "./components/Modal/ConfirmOrderModal";
+import ConfirmProductOrders from "./components/Modal/ConfirmProductOrders";
+import NewOrderBtn from "./components/Modal/NewOrderBtn";
 
 import { useState } from "react";
 
@@ -163,60 +167,6 @@ function App() {
         )}
       </ShoppingCart>
     </main>
-  );
-}
-
-function ConfirmOrderModal({ children }) {
-  return <div className="confirm-order-modal">{children}</div>;
-}
-
-function Modal({ children }) {
-  return (
-    <dialog className="modal">
-      <div>
-        <img
-          src="./assets/images/icon-order-confirmed.svg"
-          alt="order confirmed icon"
-        />
-      </div>
-      <h2>Order Confirmed</h2>
-      <p>We hope you enjoy your food!</p>
-      {children}
-    </dialog>
-  );
-}
-
-function ConfirmProductOrders({
-  productName,
-  productQuantity,
-  productPrice,
-  productTotalPrice,
-  numToString,
-  productThumbnail,
-}) {
-  console.log(productThumbnail);
-  return (
-    <>
-      <div>
-        <img src={productThumbnail} alt={productName} />
-        <div>
-          <p className="product-name">{productName}</p>
-          <p className="product-pricing-info">
-            <span className="quantity">{productQuantity}x</span>
-            <span className="text-light">@${numToString(productPrice)}</span>
-          </p>
-        </div>
-      </div>
-      <span className="overall-price">${numToString(productTotalPrice)}</span>
-    </>
-  );
-}
-
-function NewOrderBtn({ onResetOrder }) {
-  return (
-    <div className="order-btn">
-      <button onClick={() => onResetOrder()}>Start New Order</button>
-    </div>
   );
 }
 
